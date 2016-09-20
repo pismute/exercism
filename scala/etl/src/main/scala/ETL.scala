8069dcab32f50a8b91a6bc7f3683d5abae1ff1d8
Changwoo Park <pismute@gmail.com>
2016-09-21 01:59:39 +0900

    scala/ETL: change to for-comprehention ver.

--- scala/etl/src/main/scala/ETL.scala
+++ scala/etl/src/main/scala/ETL.scala
@@ -1,6 +1,7 @@
 object ETL {
   def transform(old: Map[Int, Seq[String]]) =
-    old
-      .mapValues(_.map(_.toLowerCase()))
-      .flatMap{ case (k, v) => v.map((_, k)).toMap }
+    for {
+      (k, v) <- old
+      s <- v
+    } yield (s.toLowerCase(), k)
 }
