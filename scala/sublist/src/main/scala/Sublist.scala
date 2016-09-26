917681d1fb308da731f125cd3e88e36dc1c13652
Changwoo Park <pismute@gmail.com>
2016-09-27 07:58:47 +0900

    add scala/sublist

---
+++ scala/sublist/src/main/scala/Sublist.scala
@@ -0,0 +1,16 @@
+object Sublist extends Enumeration {
+  val Equal, Unequal, Sublist, Superlist = Value
+
+  implicit class SeqHelpers(s: Seq[_]) {
+    def isSublistOf(sub: Seq[_]) =
+      if(sub.isEmpty) true
+      else s.sliding(sub.size).find(_ == sub) != None
+  }
+
+  def sublist(s: Seq[_], sub: Seq[_]) =
+    if(s == sub) Equal
+    else if(s isSublistOf sub) Superlist
+    else if(sub isSublistOf s) Sublist
+    else Unequal
+
+}
