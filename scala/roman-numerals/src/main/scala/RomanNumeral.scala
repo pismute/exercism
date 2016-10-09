3a17c36af50f1204fea5d6dfdb078de062b486c8
Changwoo Park <pismute@gmail.com>
2016-10-09 14:41:10 +0300

    update scala/roman-numerals

--- scala/roman-numerals/src/main/scala/RomanNumeral.scala
+++ scala/roman-numerals/src/main/scala/RomanNumeral.scala
@@ -1,48 +1,50 @@
-object RomanDigit {
-  type RomanDigit = String
-  val I = "I"
-  val V = "V"
-  val X = "X"
-  val L = "L"
-  val C = "C"
-  val D = "D"
-  val M = "M"
-  val empty = ""
-
-  case class Roman(position: Int, x: RomanDigit, v: RomanDigit, i: RomanDigit)
-
-  val digits =
-    Stream(
-      Roman(1000, empty, empty, M),
-      Roman(100, M, D, C),
-      Roman(10, C, L, X),
-      Roman(1, X, V, I))
-
-  implicit class IntHelper(nr: Int) {
-    def toRomanNumber = {
-      require(nr >= 0)
-      require(nr <= 3000)
-
-      digits.foldRight(Stream.empty[RomanDigit]) { (roman, acc) =>
-        ((nr / roman.position) % 10) match {
-          case 0 => acc
-          case n if n >= 1 && n <= 3 => (roman.i * n) #:: acc
-          case 4 => roman.i #:: roman.v #:: acc
-          case 5 => roman.v #:: acc
-          case n if n >= 6 && n <= 8 => roman.v #:: (roman.i * (n-5)) #:: acc
-          case 9 => roman.i #:: roman.x #:: acc
-        }
-      }
-    }
-  }
-}
+import scala.annotation.tailrec
 
 object RomanNumeral {
+  type ArabicNumber = Int
+  type RomanNumber = String
+
+  val arabicToRomans = Seq(
+    1000 -> "M",
+    900 -> "CM",
+    500 -> "D",
+    400 -> "CD",
+    100 -> "C",
+    90 -> "XC",
+    50 -> "L",
+    40 -> "XL",
+    10 -> "X",
+    9 -> "IX",
+    5 -> "V",
+    4 -> "IV",
+    1 -> "I"
+  )
+
+  def unfoldRight[A,B](seed: B)(f: B => Option[(A,B)]): Seq[A] =
+    f(seed) match {
+    case None => Seq()
+    case Some((a,b)) => a +: unfoldRight(b)(f)
+  }
+
+  implicit class ArabicNumberHelper(arabicNumber: ArabicNumber) {
+    def toRomanNumber: RomanNumber = {
+      require( arabicNumber >= 0 )
+      require( arabicNumber <= 3000 )
+
+      unfoldRight(arabicNumber){ i =>
+        arabicToRomans.find(_._1 <= i).map {
+          case (arabic, roman) => (roman, i - arabic)
+        }
+      }.mkString
+    }
+  }
+
   def apply(nr:Int) = new RomanNumeral(nr)
 }
 
-class RomanNumeral(nr: Int) {
-  import RomanDigit._
+import RomanNumeral._
 
-  lazy val value = nr.toRomanNumber.mkString
+class RomanNumeral(nr: ArabicNumber) {
+
+  lazy val value = nr.toRomanNumber
 }
