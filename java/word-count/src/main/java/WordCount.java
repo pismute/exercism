7357b6932e7dd585e92aecd6ca3a2345d233115d
Changwoo Park <pismute@gmail.com>
2016-10-09 14:39:46 +0300

    update java/word-count

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,17 +1,18 @@
 import java.util.Map;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
-
 public class WordCount {
+    public static final Pattern WORD_PATTERN = Pattern.compile("\\W+");
+
     public static <T> Collector<T, ?, Integer> countingInt() {
-        return Collectors.reducing(0, e -> 1, Integer::sum);
+        return Collectors.summingInt(e->1);
     }
 
     public Map<String, Integer> phrase(String sentences) {
-        return asList(sentences.toLowerCase().split("[^\\w]+")).stream()
+        return WORD_PATTERN.splitAsStream(sentences.toLowerCase())
                 .collect(Collectors.groupingBy(Function.identity(), countingInt()));
     }
 }
