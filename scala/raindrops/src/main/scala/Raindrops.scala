9c804e7a52455fe064929a8dbeac243ceb9339fc
Changwoo Park <pismute@gmail.com>
2016-10-10 15:52:04 +0300

    update scala/raindrops with fold

--- scala/raindrops/src/main/scala/Raindrops.scala
+++ scala/raindrops/src/main/scala/Raindrops.scala
@@ -1,29 +1,15 @@
 object Raindrops {
-  def unfoldRight[A,B](seed: B)(f: B => Option[(A,B)]): Seq[A] =
-    f(seed) match {
-      case None => Seq()
-      case Some((a,b)) => a +: unfoldRight(b)(f)
-    }
-
-  val Divisors = Stream.from(2)
-
-  def primeFactors(nr:Long) =
-    unfoldRight(nr){ seed =>
-      if(seed == 1L) None
-      else Divisors
-        .find(seed%_ == 0)
-        .map(divisor => (divisor, seed/divisor))
-  }
-
   val pliaong = Map(3 -> "Pling", 5 -> "Plang", 7 -> "Plong")
 
-  def convert(nr:Long) =
-    primeFactors(nr)
-      .distinct
-      .collect(pliaong) match {
+  def convert(nr:Long) = {
+    pliaong.foldRight(Seq.empty[String]){
+      case ((k, v), acc) if nr%k == 0 => v +: acc
+      case (_, acc) => acc
+    } match {
       case xs if xs.isEmpty => nr.toString
       case xs => xs.mkString
     }
+  }
 
   def apply() = Raindrops
 }
