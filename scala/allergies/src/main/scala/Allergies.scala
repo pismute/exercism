7f22f89aa352df50447076d4818812c6c6045e2b
Changwoo Park <pismute@gmail.com>
2016-10-10 14:45:56 +0300

    update scala/allergies with fold

--- scala/allergies/src/main/scala/Allergies.scala
+++ scala/allergies/src/main/scala/Allergies.scala
@@ -3,32 +3,25 @@ object Allergen extends Enumeration {
   val Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats = Value
 }
 
-object Allergies {
+object Allergies2 {
   import Allergen._
 
   val allergensWithScore =
     Allergen.values.toList
       .zipWithIndex
       .map{ case (allergen, i) => (allergen, math.pow(2, i).toInt) }
-      .reverse
 
   val allergensMap = allergensWithScore.toMap
 
   def isAllergicTo(allergen: Allergen, score: Int) =
     allergensMap.get(allergen).map(_ <= score).getOrElse(false)
 
-  def unfoldRight[A, B](seed: B)(f: B => Option[(A,B)]): Seq[A] =
-    f(seed) match {
-      case None => Seq()
-      case Some((a,b)) => a +: unfoldRight(b)(f)
-    }
-
-  def findAllergenWithScore(score: Int) =
-      allergensWithScore
-        .find(_._2 <= score)
-        .map{ case (a, b) => (a, score%b) }
-
-  def allergies(score: Int) = unfoldRight(score)(findAllergenWithScore).reverse
+  def allergies(score: Int) =
+    allergensWithScore.foldRight((score, Seq.empty[Allergen])){
+      case ((allergen, score), (seed, acc)) if score <= seed =>
+        (seed % score, allergen +: acc)
+      case (_, z) => z
+    }._2
 
   def apply() = Allergies
 }
