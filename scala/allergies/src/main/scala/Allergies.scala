33387c7cdbcbfdd6fb0883c0a6ddc7a00e4a15b6
Changwoo Park <pismute@gmail.com>
2016-10-14 09:43:29 +0100

    update with case object

--- scala/allergies/src/main/scala/Allergies.scala
+++ scala/allergies/src/main/scala/Allergies.scala
@@ -1,25 +1,30 @@
-object Allergen extends Enumeration {
-  type Allergen = Value
-  val Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats = Value
+object Allergen {
+  sealed trait Allergen {
+    def score:Int
+  }
+
+  case object Eggs extends Allergen { val score = math.pow(2, 0).toInt }
+  case object Peanuts extends Allergen { val score = math.pow(2, 1).toInt }
+  case object Shellfish extends Allergen { val score = math.pow(2, 2).toInt }
+  case object Strawberries extends Allergen { val score = math.pow(2, 3).toInt }
+  case object Tomatoes extends Allergen { val score = math.pow(2, 4).toInt }
+  case object Chocolate extends Allergen { val score = math.pow(2, 5).toInt }
+  case object Pollen extends Allergen { val score = math.pow(2, 6).toInt }
+  case object Cats extends Allergen { val score = math.pow(2, 7).toInt }
+
+  val values = Seq(Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats)
 }
 
-object Allergies2 {
+object Allergies {
   import Allergen._
 
-  val allergensWithScore =
-    Allergen.values.toList
-      .zipWithIndex
-      .map{ case (allergen, i) => (allergen, math.pow(2, i).toInt) }
-
-  val allergensMap = allergensWithScore.toMap
-
   def isAllergicTo(allergen: Allergen, score: Int) =
-    allergensMap.get(allergen).map(_ <= score).getOrElse(false)
+    values.find(_ == allergen).forall(_.score <= score)
 
   def allergies(score: Int) =
-    allergensWithScore.foldRight((score, Seq.empty[Allergen])){
-      case ((allergen, score), (seed, acc)) if score <= seed =>
-        (seed % score, allergen +: acc)
+    values.foldRight((score, Seq.empty[Allergen])){
+      case (allergen, (seed, acc)) if allergen.score <= seed =>
+        (seed % allergen.score, allergen +: acc)
       case (_, z) => z
     }._2
 
