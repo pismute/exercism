a490391c5557d260dee3727f93a3e1bf6522deed
Changwoo Park <pismute@gmail.com>
2016-10-17 08:25:01 +0100

    update java/pangram

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -1,24 +1,27 @@
-import java.util.Set;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
+import java.util.function.IntPredicate;
+import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-/**
- * Created by pismute on 09/10/2016.
- */
 public class Pangrams {
-    public static final Supplier<Stream<Integer>> ALPHABETS =
-            ()-> IntStream.rangeClosed((int) 'a', (int) 'z').mapToObj(Integer::new);
+    public static final IntPredicate isLowerAlphabet = i-> 'a' <= i && i <= 'z';
+
+    /*
+     * 'a' == 10
+     * 'b' == 100
+     * 'c' == 1000
+     * ... == ...
+     * 'z' == ...
+     */
+    public static final IntUnaryOperator toBitwise = i-> 2 << (i - 'a');
+
+    public static final int ALPHABETS =
+            IntStream.rangeClosed('a', 'z').map(toBitwise).sum();
 
     public static boolean isPangram(String sentence) {
-        final Set<Integer> lowercases =
-                sentence.toLowerCase().chars()
-                        .distinct()
-                        .filter(Character::isLowerCase)
-                        .mapToObj(Integer::new)
-                        .collect(Collectors.toSet());
-
-        return ALPHABETS.get().allMatch(lowercases::contains);
+        return sentence.toLowerCase().chars()
+                .filter(isLowerAlphabet)
+                .map(toBitwise)
+                .reduce(0, (a, b)-> a | b) == ALPHABETS;
     }
 }
+
