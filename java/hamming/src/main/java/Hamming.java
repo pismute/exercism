f3ad7808fbfdfb66fb10c3994ed45d3663540929
Changwoo Park <pismute@gmail.com>
2016-10-18 16:55:06 +0100

    update java/hamming

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,10 +1,11 @@
 import java.util.stream.IntStream;
 
 public class Hamming {
-    public static int compute(String l, String r) {
-        if (l.length() != r.length()) throw new IllegalArgumentException("lengths are mismatched!");
-        return IntStream.range(0, l.length())
-                .map(i -> l.charAt(i) ==  r.charAt(i) ? 0 : 1)
+    public static int compute(String left, String right) {
+        if (left.length() != right.length()) throw new IllegalArgumentException("lengths are mismatched!");
+
+        return IntStream.range(0, left.length())
+                .map(i -> left.charAt(i) ==  right.charAt(i) ? 0 : 1)
                 .sum();
     }
 }
