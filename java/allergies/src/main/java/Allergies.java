323fdffa42a538e1b58fc80cfa612310e234b1c3
Changwoo Park <pismute@gmail.com>
2016-11-05 22:44:30 +0000

    add java/allergies

---
+++ java/allergies/src/main/java/Allergies.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Allergies {
+    private final int score;
+
+    public Allergies(int score) {
+        this.score = score;
+    }
+
+    public boolean isAllergicTo(Allergen allergen) {
+        return (allergen.getScore() & score) == allergen.getScore();
+    }
+
+    public List<Allergen> getList() {
+        return Arrays.asList(Allergen.values()).stream()
+                .filter(this::isAllergicTo)
+                .collect(Collectors.toList());
+    }
+}
