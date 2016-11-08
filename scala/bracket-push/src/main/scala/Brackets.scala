74e0aa599c8d58e24d4e243254ab7530f14fa1dd
Changwoo Park <pismute@gmail.com>
2016-11-08 17:04:07 +0000

    update scala/bracket-push

--- scala/bracket-push/src/main/scala/Brackets.scala
+++ scala/bracket-push/src/main/scala/Brackets.scala
@@ -1,16 +1,29 @@
-package
+import scala.annotation.tailrec
 
 object Brackets {
-  def areBalanced(expression: String) =
-    expression
-      .foldLeft(Option(List.empty[Char])) {
-        case (Some(Nil), (']' | ')' | '}')) => None
-        case (acc, open @ ('[' | '(' | '{')) => acc.map(open +: _)
-        case (acc, ']') => acc.filter(_.head == '[').map(_.tail)
-        case (acc, ')') => acc.filter(_.head == '(').map(_.tail)
-        case (acc, '}') => acc.filter(_.head == '{').map(_.tail)
-        case (acc, _) => acc
-      }
-        .fold(false)(_.isEmpty)
+  @tailrec
+  private def foldl2[B](string: String)(start: Int, end: Int, z: B, op: (B, Char) => Option[B]): Option[B] =
+    if (start == end) Some(z)
+    else {
+      val zz = op(z, string(start))
+      if(zz.isEmpty) None
+      else foldl2(string)(start + 1, end, zz.get, op)
+    }
 
+  def foldLeftOption[B](string: String)(z: B)(op: (B, Char) => Option[B]): Option[B] =
+    foldl2(string)(0, string.length(), z, op)
+
+  def areBalanced(expression: String): Boolean =
+    foldLeftOption[List[Char]](expression)(Nil) {
+      case (Nil, ']' | ')' | '}') => None
+      case (acc, open @ ('[' | '(' | '{')) => Some(open +: acc)
+      case (acc, ']') if acc.head == '[' => Some(acc.tail)
+      case (acc, ']') => None
+      case (acc, ')') if acc.head == '(' => Some(acc.tail)
+      case (acc, ')') => None
+      case (acc, '}') if acc.head == '{' => Some(acc.tail)
+      case (acc, '}') => None
+      case (acc, _) => Some(acc)
+    }
+        .fold(false)(_.isEmpty)
 }
