124e19a20941dc4d2e11d25816f3e01552d58fa7
Changwoo Park <pismute@gmail.com>
2016-11-09 20:50:20 +0000

    update scala/bracket-push

--- scala/bracket-push/src/main/scala/Brackets.scala
+++ scala/bracket-push/src/main/scala/Brackets.scala
@@ -1,29 +1,28 @@
 import scala.annotation.tailrec
 
 object Brackets {
-  @tailrec
-  private def foldl2[B](string: String)(start: Int, end: Int, z: B, op: (B, Char) => Option[B]): Option[B] =
-    if (start == end) Some(z)
-    else {
-      val zz = op(z, string(start))
-      if(zz.isEmpty) None
-      else foldl2(string)(start + 1, end, zz.get, op)
-    }
+  val BracketPairs = Map('}' -> '{', ')' -> '(', ']' -> '[')
 
-  def foldLeftOption[B](string: String)(z: B)(op: (B, Char) => Option[B]): Option[B] =
-    foldl2(string)(0, string.length(), z, op)
+  lazy val isOpen = BracketPairs.values.toSet
+  lazy val isClose = BracketPairs.keySet
 
-  def areBalanced(expression: String): Boolean =
-    foldLeftOption[List[Char]](expression)(Nil) {
-      case (Nil, ']' | ')' | '}') => None
-      case (acc, open @ ('[' | '(' | '{')) => Some(open +: acc)
-      case (acc, ']') if acc.head == '[' => Some(acc.tail)
-      case (acc, ']') => None
-      case (acc, ')') if acc.head == '(' => Some(acc.tail)
-      case (acc, ')') => None
-      case (acc, '}') if acc.head == '{' => Some(acc.tail)
-      case (acc, '}') => None
-      case (acc, _) => Some(acc)
-    }
-        .fold(false)(_.isEmpty)
+  def isValidClose(stack: List[Char], close: Char) =
+    stack.headOption
+      .flatMap(maybeOpen => BracketPairs.get(close).map(_ == maybeOpen))
+      .getOrElse(false)
+
+  def areBalanced(expression: String): Boolean = {
+    def loop(start: Int, end: Int, stack: List[Char]): Option[List[Char]] =
+      if (start == end) Some(stack)
+      else {
+        val char = expression(start)
+
+        if(isOpen(char)) loop(start + 1, end, char +: stack)
+        else if(isValidClose(stack, char)) loop(start + 1, end, stack.tail)
+        else if(isClose(char)) None
+        else loop(start + 1, end, stack)
+      }
+
+    loop(0, expression.length, Nil).map(_.isEmpty).getOrElse(false)
+  }
 }
