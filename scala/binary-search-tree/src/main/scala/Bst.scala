819ba965bc9c24c4e0786ab45257c144c097f736
Changwoo Park <pismute@gmail.com>
2016-11-10 11:06:23 +0000

    udpate scala/binary-search-tree

--- scala/binary-search-tree/src/main/scala/Bst.scala
+++ scala/binary-search-tree/src/main/scala/Bst.scala
@@ -1,38 +1,30 @@
+case class Bst[+A](value: A, left: Option[Bst[A]] = None, right: Option[Bst[A]] = None) {
+  def insert[B >: A: Ordering](x: B): Bst[B] =
+    if(implicitly[Ordering[B]].lteq(x, value)) Bst(value, left.map(_.insert(x)).orElse(Some(Bst(x))), right)
+    else Bst(value, left, right.map(_.insert(x)).orElse(Some(Bst(x))))
+
+}
+
 object Bst {
-  trait Bst[+A] {
-    def isEmpty: Boolean
-    def value: A
-    def get = this // What does it work for?
-    def left: Bst[A]
-    def right: Bst[A]
-    def insert[B >: A : Ordering](x: B): Bst[B]
-  }
-
-  case class BstNode[+A](value: A, left: Bst[A], right: Bst[A]) extends Bst[A] {
-    def isEmpty: Boolean = false
-    def insert[B >: A : Ordering](x: B): Bst[B] =
-      if(implicitly[Ordering[B]].lteq(x, value)) BstNode(value, left.insert(x), right)
-      else BstNode(value, left, right.insert(x))
-  }
-
-  case object Empty extends Bst[Nothing] {
-    def isEmpty: Boolean = true
-    def value = throw new NoSuchElementException("value of empty Bst")
-    def left = throw new UnsupportedOperationException("left of empty Bst")
-    def right = throw new UnsupportedOperationException("right of empty Bst")
-    def insert[A: Ordering](value: A): Bst[A] = BstNode(value, Empty, Empty)
-  }
-
-  def toList[A](xs: Bst[A]) = {
+  def toList[A](xs: Bst[A]): List[A] = {
     def loop(xs:Bst[A], acc: List[A]): List[A] =
       xs match {
-        case Empty => acc
-        case BstNode(value, left, right) => loop(left, value +: loop(right, acc))
+        case Bst(value, None, None) => value +: acc
+        case Bst(value, Some(left), None) => loop(left, value +: acc)
+        case Bst(value, None, Some(right)) => value +: loop(right, acc)
+        case Bst(value, Some(left), Some(right)) => loop(left, value +: loop(right, acc))
       }
 
     loop(xs, Nil)
   }
 
-  def fromList[A: Ordering](xs: Seq[A]) = xs.foldLeft[Bst[A]](Empty)(_ insert _)
-  def apply[A: Ordering](value: A): Bst[A] = BstNode(value, Empty, Empty)
+  def fromList[A : Ordering](xs: Seq[A]): Bst[A] = {
+    require(!xs.isEmpty)
+
+    xs.foldLeft[Option[Bst[A]]](None){
+      case (None, x) => Some(Bst(x))
+      case (Some(root), x) => Some(root.insert(x))
+    }
+      .get
+  }
 }
