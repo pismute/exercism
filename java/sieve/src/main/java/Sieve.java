c934afd0d5b5f055590ad8fcd478dc87b741f351
Changwoo Park <pismute@gmail.com>
2016-11-12 14:35:18 +0000

    refactor java/sieve

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
@@ -10,28 +11,24 @@ public class Sieve {
         this.number = number;
     }
 
-    public IntStream streamWith(List<IntPredicate> filters) {
-        IntStream stream = IntStream.rangeClosed(2, number);
-
-        for(IntPredicate filter: filters) {
-            stream = stream.filter(filter);
-        }
-
-        return stream;
-    }
-
-    public List<Integer> sieves(List<IntPredicate> filters, List<Integer> acc) {
-        streamWith(filters).findFirst()
-                .ifPresent((i) -> {
-                    filters.add((ii)-> ii % i != 0);
-                    acc.add(i);
-                    sieves(filters, acc);
-                });
-
-        return acc;
+    public OptionalInt findFirst(IntPredicate sieve) {
+        return IntStream.rangeClosed(2, number)
+                .filter(sieve)
+                .findFirst();
     }
 
     public List<Integer> getPrimes() {
-        return sieves(new ArrayList<>(), new ArrayList<>());
+        List<Integer> acc = new ArrayList<>();
+        IntPredicate sieve = (i)-> true;
+
+        OptionalInt o = findFirst(sieve);
+        while(o.isPresent()) {
+            final int next = o.getAsInt();
+            acc.add(next);
+            sieve = sieve.and(i-> i % next != 0);
+            o = findFirst(sieve);
+        }
+
+        return acc;
     }
 }
