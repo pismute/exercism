d367cd13c7e4485e5278ec4480b94e9d15b3d258
Changwoo Park <pismute@gmail.com>
2016-11-17 15:55:03 +0000

    update scala/minesweeper

--- scala/minesweeper/src/main/scala/Minesweeper.scala
+++ scala/minesweeper/src/main/scala/Minesweeper.scala
@@ -1,39 +1,59 @@
 object Minesweeper {
-  val Neighbours =
-    for {
-      x <- -1 to 1
-      y <- -1 to 1 if !(x == 0 && y == 0)
-    } yield (x, y)
+  case class Point(x: Int, y: Int) {
+    def move(point: Point) = copy(x = x + point.x, y = y + point.y)
+  }
 
-  def annotate(input: Seq[String]): Seq[String] =
-    if( input.isEmpty) input
-    else {
-      val board = input.map(_.toIndexedSeq).toIndexedSeq
-      val width = board.head.size;
-      val height = board.size;
+  object Board {
+    val Neighbours: Seq[Point] =
+      for {
+        x <- -1 to 1
+        y <- -1 to 1 if !(x == 0 && y == 0)
+      } yield Point(x, y)
 
-      def isOnBoard(x: Int, y: Int): Boolean = 0 <= x && x < width && 0 <= y && y < height
-      def isMine(x: Int, y: Int): Boolean = board(y)(x) == '*'
+    def neighbours(point: Point): Seq[Point] =
+      Neighbours.map(_.move(point))
 
-      def findMine(x: Int, y: Int): Char =
-        if(isMine(x, y)) board(y)(x)
-        else {
-          val neighbors =
-            Neighbours
-              .map{ case(xx, yy) => (xx + x, yy + y) }
-              .filter{ case(xx, yy) => isOnBoard(xx, yy) }
-              .filter{ case(xx, yy) => isMine(xx,yy) }
+    def apply(input: Seq[String]) = new Board(input)
+  }
 
-          (neighbors.size + '0').toChar
-        }
+  class Board(input: Seq[String]) {
+    import Board._
 
+    require(!input.isEmpty)
+
+    val board = input.map(_.toIndexedSeq).toIndexedSeq
+    val width = board.head.size;
+    val height = board.size;
+
+    def isOnBoard(point: Point): Boolean =
+       0 <= point.x && point.x < width && 0 <= point.y && point.y < height
+
+    def isMine(point: Point): Boolean =
+      board(point.y)(point.x) == '*'
+
+    def validNeighbours(point: Point): Seq[Point] =
+      neighbours(point).filter(isOnBoard)
+
+    def countMines(point: Point): Int =
+      validNeighbours(point).filter(isMine).size
+
+    def annotatePoint(point: Point): Char =
+       if(isMine(point)) '*'
+       else countMines(point) match {
+         case 0 => ' '
+         case n => ('0' + n).toChar
+       }
+
+    def annotate: Seq[String] =
       (for {
         x <- 0 until width
         y <- 0 until height
-      } yield findMine(x, y))
-        .map(char=> if(char == '0') ' ' else char)
+      } yield annotatePoint(Point(x, y)))
         .grouped(width)
         .map(_.mkString)
         .toSeq
-    }
+  }
+
+  def annotate(input: Seq[String]): Seq[String] =
+    if(input.isEmpty) input else Board(input).annotate
 }
