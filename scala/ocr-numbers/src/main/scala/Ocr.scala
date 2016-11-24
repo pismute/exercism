cc55cc2d7bb1a0ae1a80f6e5b6e319ea987c7d01
Changwoo Park <pismute@gmail.com>
2016-11-24 06:54:23 +0000

    refactor to be able to add more letters.

--- scala/ocr-numbers/src/main/scala/Ocr.scala
+++ scala/ocr-numbers/src/main/scala/Ocr.scala
@@ -1,15 +1,11 @@
 object Ocr {
+  import OcrLetters._
+
   type Digit = (String, String, String, String)
 
-  val Digits: Map[Digit, String] =
-    toDigits(Seq(
-    " _     _  _     _  _  _  _  _ ",
-    "| |  | _| _||_||_ |_   ||_||_|",
-    "|_|  ||_  _|  | _||_|  ||_| _|",
-    "                              "))
-      .zipWithIndex
+  val Digits =
+    Seq(Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine)
       .toMap
-        .mapValues(_.toString)
         .withDefaultValue("?")
 
   def toDigit(xs: Seq[String]): Digit =
@@ -33,6 +29,69 @@ object Ocr {
   }
 
   def apply(ocr: String) = new Ocr(ocr)
+
+  object OcrLetters {
+    val Zero =
+      ((" _ ",
+        "| |",
+        "|_|",
+        "   "), "0")
+
+    val One =
+      (("   ",
+        "  |",
+        "  |",
+        "   "), "1")
+
+    val Two =
+      ((" _ ",
+        " _|",
+        "|_ ",
+        "   "), "2")
+
+    val Three =
+      ((" _ ",
+        " _|",
+        " _|",
+        "   "), "3")
+
+    val Four =
+      (("   ",
+        "|_|",
+        "  |",
+        "   "), "4")
+
+    val Five =
+      ((" _ ",
+        "|_ ",
+        " _|",
+        "   "), "5")
+
+    val Six =
+      ((" _ ",
+        "|_ ",
+        "|_|",
+        "   "), "6")
+
+    val Seven =
+      ((" _ ",
+        "  |",
+        "  |",
+        "   "), "7")
+
+    val Eight =
+      ((" _ ",
+        "|_|",
+        "|_|",
+        "   "), "8")
+
+    val Nine =
+      ((" _ ",
+        "|_|",
+        " _|",
+        "   "), "9")
+  }
+
 }
 
 class Ocr(val ocr: String) {
