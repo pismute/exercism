d6e0b549bb36831ab4977c59064b2d090b415fd1
Changwoo Park <pismute@gmail.com>
2016-12-05 09:13:34 -0500

    get rid of one case.

--- scala/pig-latin/src/main/scala/PigLatin.scala
+++ scala/pig-latin/src/main/scala/PigLatin.scala
@@ -2,13 +2,13 @@ package
 
 object PigLatin {
   val Vowels = "^([aeiou])(.*)".r
-  val Consonants = "^(ch|thr|th|sch|qu|squ)(.*)".r
+  val Consonants = "^(ch|thr|th|sch|qu|squ|[^aeiou])(.*)".r
 
   def ay(plain: String): String =
     plain match {
-      case Vowels(startWith, _) => plain + "ay"
-      case Consonants(startWith, xs) => xs + startWith + "ay"
-      case _ => plain.tail + plain.head + "ay"
+      case Vowels(startWith, xs) => plain  + "ay"
+      case Consonants(startWith, xs) =>
+        xs + startWith + "ay"
     }
 
   def translate(plain: String): String =
