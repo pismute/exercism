cfa646d5e1baf35ce8533537b0374c5a979d8441
Changwoo Park <pismute@gmail.com>
2016-12-06 14:08:42 -0500

    make one of the simplest PigLatin

--- scala/pig-latin/src/main/scala/PigLatin.scala
+++ scala/pig-latin/src/main/scala/PigLatin.scala
@@ -1,14 +1,13 @@
 package
 
 object PigLatin {
-  val Vowels = "^([aeiou])(.*)".r
-  val Consonants = "^(ch|thr|th|sch|qu|squ|.)(.*)".r
+  val Consonants = "^(ch|thr|th|sch|qu|squ|[^aeiou])(.*)".r
 
   def ay(plain: String): String =
     plain match {
-      case Vowels(startWith, xs) => plain  + "ay"
       case Consonants(startWith, xs) =>
         xs + startWith + "ay"
+      case _ => plain + "ay"
     }
 
   def translate(plain: String): String =
