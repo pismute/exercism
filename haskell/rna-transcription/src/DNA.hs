1b8ead6cde19eae21bf63ce6092d2213a13b50dc
Changwoo Park <pismute@gmail.com>
2016-12-22 07:39:44 -0500

    remove containers dependancy as removing Data.Map

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,19 +1,15 @@
 module DNA (toRNA) where
 
-import qualified Data.Map as Map
-import qualified Data.List as List
-
 rnaStrand =
-  Map.fromList
-    [('G', 'C'),
-     ('C', 'G'),
-     ('T', 'A'),
-     ('A', 'U')]
+  [('G', 'C'),
+   ('C', 'G'),
+   ('T', 'A'),
+   ('A', 'U')]
 
 toRNA :: String -> Maybe String
-toRNA = (foldr combine $ Just []) . (fmap lookup)
+toRNA = (foldr combine $ Just []) . (fmap find)
   where
-    lookup = (`Map.lookup` rnaStrand)
+    find = (`lookup` rnaStrand)
     combine maybe1 maybe2 =
       do m1 <- maybe1
          m2 <- maybe2
