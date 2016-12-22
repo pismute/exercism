c10fe19b5f9b9395370dd89752513dbdc870fb9b
Changwoo Park <pismute@gmail.com>
2016-12-22 16:28:54 -0500

    introducing `mapM` and `traverse`

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -7,10 +7,5 @@ rnaStrand =
    ('A', 'U')]
 
 toRNA :: String -> Maybe String
-toRNA = (foldr combine $ Just []) . (fmap find)
-  where
-    find = (`lookup` rnaStrand)
-    combine maybe1 maybe2 =
-      do m1 <- maybe1
-         m2 <- maybe2
-         return (m1 : m2)
+-- toRNA = mapM (`lookup` rnaStrand)
+toRNA = traverse (`lookup` rnaStrand)
