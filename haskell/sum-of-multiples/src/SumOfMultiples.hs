a1de1aab1b2a14d57762fd20f973bf016b6f34c7
Changwoo Park <pismute@gmail.com>
2016-12-25 08:34:29 -0500

    add haskell/sum-of-multiples

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,10 @@
 module SumOfMultiples (sumOfMultiples) where
 
+import Data.List as List
+
+sumOfMultiples :: Integral a => [a] -> a -> a
 sumOfMultiples xs limit =
-  xs
+  sum $ List.nub $ xs >>= (`multiples` limit)
+  where
+    multiples x limit =
+      (filter (/=limit) . map (*x)) [1..(limit `div` x)]
