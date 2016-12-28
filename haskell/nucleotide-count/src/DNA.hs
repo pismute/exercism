da3631a28dcda7cbb30747f834b56e757514c8f2
Changwoo Park <pismute@gmail.com>
2016-12-28 12:23:29 -0500

    check elements of a Set instead of a List

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,13 +1,17 @@
 module DNA (count, nucleotideCounts) where
 
 import Data.Map (Map, fromList)
+import qualified Data.Set as Set
 
 acgt = "ACGT"
+acgtSet = Set.fromList acgt
 
 count :: Char -> String -> Either String Int
 count nucleotide nucleotides
-  | all (`elem` acgt) (nucleotide : nucleotides) = (Right . length . filter (==nucleotide)) nucleotides
-  | otherwise = Left nucleotides
+  | all (`elem` acgtSet) (nucleotide : nucleotides) =
+    (Right . length . filter (==nucleotide)) nucleotides
+  | otherwise =
+    Left nucleotides
 
 nucleotideCounts :: String -> Either String (Map Char Int)
 nucleotideCounts nucleotides = fromList <$> (acgt `zip`) <$> traverse (`count` nucleotides) acgt
