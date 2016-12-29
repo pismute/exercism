55be3fa43192f43d94bfbc55fcb4d7039709b17d
Changwoo Park <pismute@gmail.com>
2016-12-29 15:00:55 -0500

    reimplement without length

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,6 +1,15 @@
 module Hamming (distance) where
 
+zipMayAll :: Eq a => [a] -> [a] -> [(Maybe a, Maybe a)]
+zipMayAll xs ys =
+  takeWhile (/=(Nothing, Nothing)) $ zip (infin xs) (infin ys)
+  where
+    infin = (++(repeat Nothing)) . (Just <$>)
+
+distance :: Eq a => [a] -> [a] -> Maybe Int
 distance xs ys =
-  if length xs /= length ys
-    then Nothing
-    else Just $ length $ filter (uncurry (/=)) $ zip xs ys
+  length . filter (uncurry (/=)) <$> (mapM valid $ zipMayAll xs ys)
+  where
+    valid (_, Nothing) = Nothing
+    valid (Nothing, _) = Nothing
+    valid (Just x, Just y) = Just (x,y)
