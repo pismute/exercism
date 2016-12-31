0a5a44a025c1b3618c41ae9afe7d0e157d427f63
Changwoo Park <pismute@gmail.com>
2016-12-31 11:01:02 -0500

    going to pointfree

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -2,16 +2,14 @@ module School (School, add, empty, grade, sorted) where
 
 import Data.List (sort)
 import qualified Data.Map as M
+import Control.Applicative ((<|>))
 
 type School = M.Map Int [String]
 type Grades = [(Int, [String])]
 
 add :: Int -> String -> School -> School
-add grade name school =
-  M.alter append grade school
-  where
-    append (Nothing) = Just [name]
-    append (Just xs) = Just $ xs ++ [name]
+add grade name =
+  M.alter (fmap (++ [name]) . (<|> Just [])) grade
 
 empty :: School
 empty = M.empty
@@ -20,4 +18,4 @@ grade :: Int -> School -> [String]
 grade = M.findWithDefault []
 
 sorted :: School -> Grades
-sorted = ((sort <$>) <$>) . M.toAscList
+sorted = M.toAscList . M.map sort
