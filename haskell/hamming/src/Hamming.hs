b9e6d92b9e43693ff592824ef0c4945551115b35
Changwoo Park <pismute@gmail.com>
2017-01-01 06:58:27 -0500

    change name 'with' to 'f' and reduce case.

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,11 +1,10 @@
 module Hamming (distance) where
 
 zipWithExactMay :: (a -> b -> c) -> [a] -> [b] -> Maybe [c]
-zipWithExactMay with [] [] = Just []
-zipWithExactMay with [] _ = Nothing
-zipWithExactMay with _ [] = Nothing
-zipWithExactMay with (x:xs) (y:ys) =
-  (with x y: ) <$> (zipWithExactMay with xs ys)
+zipWithExactMay f [] [] = Just []
+zipWithExactMay f (x:xs) (y:ys) =
+  (f x y: ) <$> (zipWithExactMay f xs ys)
+zipWithExactMay f _ _ = Nothing
 
 distance :: Eq a => [a] -> [a] -> Maybe Int
 distance xs ys =
