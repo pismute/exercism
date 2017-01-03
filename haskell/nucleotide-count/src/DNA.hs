50791572f8cdd6fde3a32d249da44f9f2379bd6d
Changwoo Park <pismute@gmail.com>
2017-01-03 17:09:57 -0500

    correct Either usage and count as single pass

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,17 +1,23 @@
 module DNA (count, nucleotideCounts) where
 
-import Data.Map (Map, fromList)
-import qualified Data.Set as Set
+import qualified Data.Map as M
+import qualified Data.Set as S
 
-acgt = "ACGT"
-acgtSet = Set.fromList acgt
+acgt = S.fromList "ACGT"
+zeroNucleotides = M.fromList $ (\x-> (x, 0)) <$> (S.toList acgt)
+
+validNucleotide :: Char -> Either String Char
+validNucleotide nucleotide
+  | elem nucleotide acgt = Right nucleotide
+  | otherwise = Left $ "unknown nucleotide: " ++ [nucleotide]
+
+nucleotideCounts :: String -> Either String (M.Map Char Int)
+nucleotideCounts nucleotides = (foldr increment zeroNucleotides) <$> traverse validNucleotide nucleotides
+  where
+    increment = flip (M.insertWith (+)) 1
 
 count :: Char -> String -> Either String Int
-count nucleotide nucleotides
-  | all (`elem` acgtSet) (nucleotide : nucleotides) =
-    (Right . length . filter (==nucleotide)) nucleotides
-  | otherwise =
-    Left nucleotides
-
-nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts nucleotides = fromList <$> (acgt `zip`) <$> traverse (`count` nucleotides) acgt
+count nucleotide nucleotides =
+  do x <- validNucleotide nucleotide
+     xs <- nucleotideCounts nucleotides
+     return $ xs M.! x
