81be3965b0a42f9b18ea7df807bfc13c1ad13312
Changwoo Park <pismute@gmail.com>
2017-01-03 17:33:35 -0500

    introduce `TupleSections`, (M.adjust succ) and M.member

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,23 +1,24 @@
+{-# LANGUAGE TupleSections #-}
 module DNA (count, nucleotideCounts) where
 
 import qualified Data.Map as M
 import qualified Data.Set as S
 
-acgt = S.fromList "ACGT"
-zeroNucleotides = M.fromList $ (\x-> (x, 0)) <$> (S.toList acgt)
+zeroNucleotides = M.fromList $ (, 0) <$> "ACGT"
 
 validNucleotide :: Char -> Either String Char
 validNucleotide nucleotide
-  | elem nucleotide acgt = Right nucleotide
+  | M.member nucleotide zeroNucleotides = Right nucleotide
   | otherwise = Left $ "unknown nucleotide: " ++ [nucleotide]
 
 nucleotideCounts :: String -> Either String (M.Map Char Int)
-nucleotideCounts nucleotides = (foldr increment zeroNucleotides) <$> traverse validNucleotide nucleotides
+nucleotideCounts nucleotides =
+  (foldr increment zeroNucleotides) <$> traverse validNucleotide nucleotides
   where
-    increment = flip (M.insertWith (+)) 1
+    increment = M.adjust succ
 
 count :: Char -> String -> Either String Int
-count nucleotide nucleotides =
-  do x <- validNucleotide nucleotide
-     xs <- nucleotideCounts nucleotides
-     return $ xs M.! x
+count nucleotide nucleotides = do
+  x <- validNucleotide nucleotide
+  xs <- nucleotideCounts nucleotides
+  return $ xs M.! x
