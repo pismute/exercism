04551bbdf70d65baead7c8c416996137fd25db2c
Changwoo Park <pismute@gmail.com>
2017-01-09 10:30:44 -0500

    simplify transforming with `do` notation.

--- haskell/etl/src/ETL.hs
+++ haskell/etl/src/ETL.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE TupleSections #-}
 module ETL (transform) where
 
 import qualified Data.Map as M
 import qualified Data.Char as C
 
 transform :: M.Map a String -> M.Map Char a
-transform = M.fromList . (>>= uncurry transformOne) . M.toList . ((map C.toLower) <$>)
-  where
-    transformOne a = ((, a) <$>)
+transform xs = M.fromList $ do
+  (k, ys) <- M.toList xs
+  y <- ys
+  return (C.toLower y, k)
