d28579bf5d9025a62d59bc147a1b34309bc8be38
Changwoo Park <pismute@gmail.com>
2017-01-25 20:00:30 -0500

    reimplement by new interface

--- scala/rna-transcription/src/main/scala/Dna.scala
+++ scala/rna-transcription/src/main/scala/Dna.scala
@@ -1,12 +1,22 @@
+import scala.annotation.tailrec
+
 object Dna {
   val Transcription =
     Map('G' -> 'C', 'C' -> 'G', 'T' -> 'A', 'A' -> 'U')
 
-  def apply() = new Dna
-}
+  def toRna(xs: String) = traverse(Transcription.get, xs)
 
-class Dna {
-  import Dna._
+  def traverse(f: Char => Option[Char], xs: String): Option[String] = {
+    @tailrec
+    def _loop(xs: Seq[Char], acc: Seq[Option[Char]]): Option[Seq[Char]] =
+      if(xs.isEmpty) Option(acc.reverse.map(_.get))
+      else
+        f(xs.head) match {
+          case None => None
+          case x => _loop(xs.tail, x +: acc)
+        }
 
-  def toRna(s: String) = s.map(Transcription)
+    _loop(xs.seq, Nil).map(_.mkString)
+  }
+
 }
