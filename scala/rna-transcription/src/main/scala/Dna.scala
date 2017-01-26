20f7ecbdf72e3950c2afec440cbaa4eb410cee3d
Changwoo Park <pismute@gmail.com>
2017-01-25 20:10:56 -0500

    drop reverse

--- scala/rna-transcription/src/main/scala/Dna.scala
+++ scala/rna-transcription/src/main/scala/Dna.scala
@@ -1,5 +1,3 @@
-import scala.annotation.tailrec
-
 object Dna {
   val Transcription =
     Map('G' -> 'C', 'C' -> 'G', 'T' -> 'A', 'A' -> 'U')
@@ -7,16 +5,15 @@ object Dna {
   def toRna(xs: String) = traverse(Transcription.get, xs)
 
   def traverse(f: Char => Option[Char], xs: String): Option[String] = {
-    @tailrec
-    def _loop(xs: Seq[Char], acc: Seq[Option[Char]]): Option[Seq[Char]] =
-      if(xs.isEmpty) Option(acc.reverse.map(_.get))
+    def _loop(xs: Seq[Char]): Option[Seq[Char]] =
+      if(xs.isEmpty) Option(Nil)
       else
         f(xs.head) match {
           case None => None
-          case x => _loop(xs.tail, x +: acc)
+          case Some(x) => _loop(xs.tail).map(x +: _)
         }
 
-    _loop(xs.seq, Nil).map(_.mkString)
+    _loop(xs.seq).map(_.mkString)
   }
 
 }
