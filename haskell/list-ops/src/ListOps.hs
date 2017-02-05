aada31dad01cc30c10ada35e48e8912b2f8025b1
Changwoo Park <pismute@gmail.com>
2017-02-05 15:00:05 +0000

    add haskell/list-ops

---
+++ haskell/list-ops/src/ListOps.hs
@@ -0,0 +1,43 @@
+module ListOps
+  ( length
+  , reverse
+  , map
+  , filter
+  , foldr
+  , foldl'
+  , (++)
+  , concat
+  ) where
+
+import Prelude hiding
+  ( length, reverse, map, filter, foldr, (++), concat )
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' _ z [] = z
+foldl' f z (x : xs) = let
+  z' = f z x
+  in z' `seq` foldl' f z' xs
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr _ z [] = z
+foldr f z (x : xs) = f x $ foldr f z xs
+
+length :: [a] -> Int
+length = foldr ((+) . const 1) 0
+
+reverse :: [a] -> [a]
+reverse = foldl' (flip (:)) []
+
+map :: (a -> b) -> [a] -> [b]
+map f = foldr ((:) . f) []
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter f = foldr (is f) []
+  where
+    is g x xs = if g x then x : xs else xs
+
+(++) :: [a] -> [a] -> [a]
+xs ++ ys = foldr (:) ys xs
+
+concat :: [[a]] -> [a]
+concat = foldr (++) []
