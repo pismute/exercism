f7dbd57684acfacfaa8da0df0fca7703a86a0613
Changwoo Park <pismute@gmail.com>
2017-02-05 15:12:50 +0000

    try with BangPatterns

--- haskell/list-ops/src/ListOps.hs
+++ haskell/list-ops/src/ListOps.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module ListOps
   ( length
   , reverse
@@ -13,10 +14,8 @@ import Prelude hiding
   ( length, reverse, map, filter, foldr, (++), concat )
 
 foldl' :: (b -> a -> b) -> b -> [a] -> b
-foldl' _ z [] = z
-foldl' f z (x : xs) = let
-  z' = f z x
-  in z' `seq` foldl' f z' xs
+foldl' _ !z [] = z
+foldl' f !z (x : xs) = foldl' f (f z x) xs
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
 foldr _ z [] = z
