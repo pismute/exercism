9be04a787f85c38f5c9f94c2a798e565f2aa7600
Changwoo Park <pismute@gmail.com>
2017-02-20 12:06:05 +0100

    try with `unfoldr`

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -1,13 +1,15 @@
 module Triangle (rows) where
 
+import qualified Data.List as L
 import qualified Data.List.Split as LS
 
 rows :: Int -> [[Integer]]
-rows n = reverse $ foldr f' [] [0..(n-1)]
+rows n = L.unfoldr (uncurry f') (n, [])
   where
     next' [] = [1]
     next' xs = [1] ++ (sum <$> LS.divvy 2 1 xs) ++ [1]
-    f' _ xs = (:xs) . next' $
-      if null xs
-        then []
-        else head xs
+    f' n xs
+      | n <= 0 = Nothing
+      | otherwise = let
+        ys = next' xs
+        in Just (ys, (n - 1, ys))
