70090f27e7e7e2245d2868d7f2c704e8aa0c2306
Changwoo Park <pismute@gmail.com>
2017-02-26 07:02:42 +0100

    go more functional.

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,21 +1,36 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Matrix {
-    private final int[][] matrix;
+    private final int[][] rows;
+    private final int[][] cols;
 
     public Matrix(String xss) {
-        this.matrix = parseLines(xss);
+        final int[][] matrix = parseLines(xss);
+        this.rows = matrix;
+        this.cols = transpose(matrix);
+    }
+
+    public int getRowsCount() {
+        return rows.length;
+    }
+
+    public int getColumnsCount() {
+        return cols.length;
+    }
+
+    public int[] getRow(int i) {
+        return rows[i];
+    }
+
+    public int[] getColumn(int i) {
+        return cols[i];
     }
 
     public static int[][] parseLines(String xss) {
-        String[] xs = xss.split("\n");
-        final int[][] matrix = new int[xs.length][];
-
-        for(int i = 0; i < xs.length; i++ ) {
-            matrix[i] = parseLine(xs[i]);
-        }
-
-        return matrix;
+        return Arrays.stream(xss.split("\n"))
+                .map(Matrix::parseLine)
+                .toArray(int[][]::new);
     }
 
     public static int[] parseLine(String xs) {
@@ -24,27 +39,16 @@ public class Matrix {
                 .toArray();
     }
 
-
-    public int getRowsCount() {
-        return matrix.length;
+    public static int[][] transpose(int[][] xss) {
+        return IntStream.range(0, xss[0].length)
+                .mapToObj(i -> transposeColumn(xss, i))
+                .toArray(int[][]::new);
     }
 
-    public int getColumnsCount() {
-        return matrix[0].length;
+    public static int[] transposeColumn(int[][] xss, int i) {
+        return IntStream.range(0, xss.length)
+                .map(j -> xss[j][i])
+                .toArray();
     }
 
-    public int[] getRow(int i) {
-        return Arrays.copyOf(matrix[i], matrix[i].length);
-    }
-
-    public int[] getColumn(int i) {
-        final int nRow = matrix.length;
-        final int[] column = new int[nRow];
-
-        for(int j = 0; j < nRow; j++) {
-            column[j] = matrix[j][i];
-        }
-
-        return column;
-    }
 }
