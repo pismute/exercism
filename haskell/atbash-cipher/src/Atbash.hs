affe29489a60be9917bd34f1fe559bd6d5f34b23
Changwoo Park <pismute@gmail.com>
2017-02-28 15:07:51 +0100

    try with `findWithDefault`

--- haskell/atbash-cipher/src/Atbash.hs
+++ haskell/atbash-cipher/src/Atbash.hs
@@ -1,6 +1,5 @@
 module Atbash (decode, encode) where
 
-import Data.Maybe (fromMaybe)
 import qualified Data.Map as M
 import qualified Data.Char as C
 import qualified Data.List.Split as LS
@@ -11,17 +10,17 @@ plain = ['a'..'z']
 dic :: M.Map Char Char
 dic = M.fromList $ zip plain (reverse plain)
 
-withId :: (Ord a) => a -> M.Map a a -> a
-withId x = fromMaybe x <$> M.lookup x
+findWithId :: (Ord a) => a -> M.Map a a -> a
+findWithId = M.findWithDefault <*> id
 
 isLetterOrNumber :: Char -> Bool
 isLetterOrNumber = (||) <$> C.isLetter <*> C.isNumber
 
 decode :: String -> String
-decode = map (`withId` dic) . filter isLetterOrNumber
+decode = map (`findWithId` dic) . filter isLetterOrNumber
 
 encode :: String -> String
 encode = group' 5 . encode' . filter isLetterOrNumber
   where
     group' n = unwords . LS.chunksOf n
-    encode' = map ((`withId` dic) . C.toLower)
+    encode' = map ((`findWithId` dic) . C.toLower)
