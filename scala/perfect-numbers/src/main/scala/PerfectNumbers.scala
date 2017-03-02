a0053e1f57c1db0eb2ae442b09c410903c6ec8b6
Changwoo Park <pismute@gmail.com>
2017-03-02 12:36:11 +0100

    change with the algorithm on Wikipedia

--- scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
+++ scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
@@ -7,14 +7,13 @@ object PerfectNumbers {
   import NumberType._
 
   def sumFactorsOf(n: Int) : Int =
-    (2 to math.sqrt(n).toInt)
+    (1 to math.sqrt(n).toInt)
       .filter( n % _ == 0 )
-      .flatMap(x => Seq(x, n / x))
-      .sum + 1
+      .map( x => x + n/x)
+      .sum / 2
 
   def classify(n: Int) : NumberType =
     sumFactorsOf(n) match {
-      case 1 => Deficient
       case x if x == n => Perfect
       case x if x > n => Abundant
       case x if x < n => Deficient
