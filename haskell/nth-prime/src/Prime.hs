1a452d20591e26f1f8987b473e92b652c1c5dddc
Changwoo Park <pismute@gmail.com>
2017-03-03 08:22:11 +0100

    add haskell/nth-prime

---
+++ haskell/nth-prime/src/Prime.hs
@@ -0,0 +1,12 @@
+module Prime (nth) where
+
+import Control.Monad (guard)
+
+nth :: Int -> Maybe Integer
+nth n = do
+  guard $ n > 0
+  return . head . drop (n-1) $ filter isPrime [2..]
+  where
+    sqrtFloor = floor . (sqrt :: Double -> Double) . fromIntegral
+    isFactorOf x = (==0) . (x `mod`)
+    isPrime x = not $ any (isFactorOf x) [2..(sqrtFloor x)]
