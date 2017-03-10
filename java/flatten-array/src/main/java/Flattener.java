22e6646fe3ef924830d5e94104543abb7aa465a4
Changwoo Park <pismute@gmail.com>
2017-03-10 18:36:46 +0100

    drop static

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -3,17 +3,17 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 final class Flattener {
-    
-    public static Stream<Object> enflat(Object x) {
+
+    public Stream<Object> enflat(Object x) {
         if(x == null ) return Stream.empty();
         else if(x instanceof List) {
             return ((List) x).stream()
-                    .flatMap(Flattener::enflat);
+                    .flatMap(this::enflat);
         }
         else return Stream.of(x);
     }
 
-    public static List<Object> flatten(List<Object> xs) {
+    public List<Object> flatten(List<Object> xs) {
         return enflat(xs).collect(Collectors.toList());
     }
 }
