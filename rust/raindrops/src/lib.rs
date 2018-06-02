958e1c3db15272f834ce1b13518d5eea5cc1fce9
Changwoo Park <pismute@gmail.com>
2018-06-02 07:38:12 +0200

    upddate rust/raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(slice_patterns)]
+
 // immutable ?
 static FACTORS: [(usize,&str); 3] = [(3, "Pling"), (5, "Plang"), (7, "Plong")];
 
@@ -7,6 +9,9 @@ pub fn raindrops(n: usize) -> String {
         .flat_map(|x| if (n % x.0) == 0 {vec!(x.1)} else {vec!()})
         .collect();
 
-    // pattern matching ?
-    if xs.is_empty() {n.to_string()} else {xs.concat()}
+    //if xs.is_empty() {n.to_string()} else {xs.concat()}
+    match xs.as_slice() {
+        [] => n.to_string(),
+        ys => ys.concat()
+    }
 }
