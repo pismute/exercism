17b35682edcd7b7a866cb3d124e56b7c2ac762d8
Changwoo Park <pismute@gmail.com>
2022-01-12 17:07:33 +0100

    rust/nth-prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,13 +1,8 @@
 fn is_prime(n: u32) -> bool {
     let to = (n as f64).sqrt() as u32 + 1;
-
     !(2..to).any(|x| n % x == 0)
 }
 
-pub fn nth(n: u32) -> Option<u32> {
-    if n < 1 {
-        None
-    } else {
-        (2..).filter(|x| is_prime(*x)).nth((n-1) as usize)
-    }
+pub fn nth(n: u32) -> u32 {
+    (2..).filter(|x| is_prime(*x)).nth(n as usize).unwrap()
 }
