297df3406f540c0082a2f7a7ff6254d580d8713f
Changwoo Park <pismute@gmail.com>
2022-01-30 12:02:23 +0100

    fix unfoldr in rust

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,33 +1,33 @@
-struct Unfold<B, F> {
-    seed: B,
+struct Unfoldr<A, F> {
+    seed: A,
     f: F,
 }
 
-impl<A, B, F> Iterator for Unfold<B, F>
+impl<A, B, F> Iterator for Unfoldr<A, F>
 where
-    F: FnMut(&B) -> Option<(A, B)>,
+    F: FnMut(&A) -> Option<(A, B)>,
 {
-    type Item = A;
+    type Item = B;
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        let (item, seed) = (self.f)(&self.seed)?;
+        let (seed, item) = (self.f)(&self.seed)?;
         self.seed = seed;
         Some(item)
     }
 }
 
-fn unfold<A, B, F>(seed: B, f: F) -> Unfold<B, F>
+fn unfoldr<A, B, F>(seed: A, f: F) -> Unfoldr<A, F>
 where
-    F: FnMut(&B) -> Option<(A, B)>,
+    F: FnMut(&A) -> Option<(A, B)>,
 {
-    Unfold { seed, f }
+    Unfoldr { seed, f }
 }
 
 pub fn is_armstrong_number(num: u32) -> bool {
     let len = (num as f64).log10() as u32 + 1;
 
-    let armstrong: u32 = unfold(num, |x| if *x < 1 { None } else { Some((x % 10, x / 10)) })
+    let armstrong: u32 = unfoldr(num, |x| if *x < 1 { None } else { Some((x / 10, x % 10)) })
         .map(|x| x.pow(len))
         .sum();
 
--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,34 +1,33 @@
-struct Unfold<B, F> {
-    seed: B,
+struct Unfoldr<A, F> {
+    seed: A,
     f: F,
 }
 
-impl<A, B, F> Iterator for Unfold<B, F>
+impl<A, B, F> Iterator for Unfoldr<A, F>
 where
-    F: FnMut(&B) -> Option<(A, B)>,
+    F: FnMut(&A) -> Option<(A, B)>,
 {
-    type Item = A;
+    type Item = B;
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        let (item, seed) = (self.f)(&self.seed)?;
+        let (seed, item) = (self.f)(&self.seed)?;
         self.seed = seed;
         Some(item)
     }
 }
 
-/// std::iter::successors is not the best.
-fn unfold<A, B, F>(seed: B, f: F) -> Unfold<B, F>
+fn unfoldr<A, B, F>(seed: A, f: F) -> Unfoldr<A, F>
 where
-    F: FnMut(&B) -> Option<(A, B)>,
+    F: FnMut(&A) -> Option<(A, B)>,
 {
-    Unfold { seed, f }
+    Unfoldr { seed, f }
 }
 
 pub fn find<A: Ord, B: AsRef<[A]>>(array: B, key: A) -> Option<usize> {
     let arr = array.as_ref();
 
-    unfold((0, arr.len()), |(start, end)| {
+    unfoldr((0, arr.len()), |(start, end)| {
         let i = start + (end - start) / 2;
 
         if start == end {
@@ -36,11 +35,11 @@ pub fn find<A: Ord, B: AsRef<[A]>>(array: B, key: A) -> Option<usize> {
         } else {
             arr.get(i).map(|x| {
                 if *x == key {
-                    (Some(i), (0, 0))
+                    ((0, 0), Some(i))
                 } else if *x < key {
-                    (None, (i + 1, *end))
+                    ((i + 1, *end), None)
                 } else {
-                    (None, (*start, i))
+                    ((*start, i), None)
                 }
             })
         }
