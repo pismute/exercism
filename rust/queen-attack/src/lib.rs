0d58deddd25e55183b364d937ffcc2840891af74
Changwoo Park <pismute@gmail.com>
2022-02-03 23:00:44 +0100

    rust/queen-attack

---
+++ rust/queen-attack/src/lib.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+pub struct ChessPosition(i32, i32);
+
+#[derive(Debug)]
+pub struct Queen(ChessPosition);
+
+impl ChessPosition {
+    pub fn new(rank: i32, file: i32) -> Option<Self> {
+        match (rank, file) {
+            ((0..=7), (0..=7)) => Some(ChessPosition(rank, file)),
+            _ => None,
+        }
+    }
+}
+
+impl Queen {
+    pub fn new(position: ChessPosition) -> Self {
+        Queen(position)
+    }
+
+    pub fn can_attack(&self, other: &Queen) -> bool {
+        let dx = self.0 .0 - other.0 .0;
+        let dy = self.0 .1 - other.0 .1;
+        dx == 0 || dy == 0 || (dx / dy).abs() == 1
+    }
+}
